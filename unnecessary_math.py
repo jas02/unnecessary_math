5fb5b48cbbe11e3a0de9aacdf69f5697f337c473
Lumír Jasiok <lumir.jasiok@tieto.com>
2020-03-31 16:29:52 +0200

    Initial version of unnecessary math

---
+++ unnecessary_math.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+#  Copyright (C) 2020 Tieto Czech s.r.o.
+#  Lumir Jasiok
+#  lumir.jasiok@tieto.com
+#  http://www.tieto.com
+#
+#
+#
+
+def multiply(a, b):
+    return a * b
+
